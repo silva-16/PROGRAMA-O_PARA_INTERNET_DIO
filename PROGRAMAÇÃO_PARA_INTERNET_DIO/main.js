0850c0562c56ea682081564f4072a4d64d026ec3
silva-16 <68476760+silva-16@users.noreply.github.com>
2021-01-21 09:22:53 -0300

    Add files via upload

---
+++ PROGRAMAÇÃO_PARA_INTERNET_DIO/main.js
@@ -0,0 +1,68 @@
+//AULA 03 Condicionais, Laços de repetição e Date 
+
+
+
+console.log(1425+"-"+1519);
+
+
+//AULA 02 Array e dicionários
+
+/*
+
+  // Lisata com dicionário
+
+  var frutas =  [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]; 
+  console.log(frutas);
+
+  
+  // DICIONÁRIO
+
+       var fruta =  {nome: "maça", cor: "vermelha"};
+       console.log(fruta);
+  
+    // ARRY
+
+  var lista = ["maça","pera", "laranja"];
+
+  //adicionar um intem a lista
+  lista.push("uva");
+
+  //Retitar o último item da lista
+  lista.pop();
+
+// "imprimir na tela"
+console.log(lista);
+
+// Mostra o tamanho da lista
+console.log(lista.length);
+
+// Mostra a lista da formas invertida
+console.log(lista.reverse());
+
+// Imprime as informações no modo sting, ou seja o que era
+// para ser impresso no formato de array agora será uma string
+
+console.log(lista.toString());
+
+// Transforma em string porém da a opção de imprimir separado por outos
+// caracteres 
+
+console.log(lista.join(" - "));
+ 
+ 
+ */
+ 
+ 
+ // Aula 01
+
+ /*
+ var nome = "Marcos Mariano da Silva Junior"
+ var idade = 24;
+ var idade2 = 10;
+ var total = idade+idade2;
+ console.log(idade);
+ console.log(idade2);
+ alert("Bunda lele"+nome+" "+"minha idade é"+idade);
+ alert("idade1 "+idade+" idade2 "+idade2+" total = "+ total);
+ 
+ */
